109df9587f084f85c7f3e13a5eca75d18b0d7ce1
FranChesKo <khl_doss@mail.ru>
2024-02-04 09:19:16 +0800

    Add delete queries

--- src/database/queries/__init__.py
+++ src/database/queries/__init__.py
@@ -1,9 +1,11 @@
 from .queries import create_tables
 from . import insert
 from . import update
+from . import delete
 
 __all__ = [
     "create_tables",
+    "delete",
     "insert",
     "update"
 ]
