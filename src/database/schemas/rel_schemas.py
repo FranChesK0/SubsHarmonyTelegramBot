cfe43d3dd6a0bf002c145c813eeccd69a33524c0
FranChesKo <khl_doss@mail.ru>
2024-02-04 08:52:14 +0800

    Fix rel schemas inheritance

--- src/database/schemas/rel_schemas.py
+++ src/database/schemas/rel_schemas.py
@@ -1,14 +1,14 @@
 from database import schemas
 
 
-class UserRelDTO(schemas.UserDTO, schemas.base.RelDTO):
+class UserRelDTO(schemas.UserDTO, schemas.base.BaseRelDTO):
     subscriptions: list[schemas.SubscriptionDTO]
 
 
-class SubscriptionRelDTO(schemas.SubscriptionDTO, schemas.base.RelDTO):
+class SubscriptionRelDTO(schemas.SubscriptionDTO, schemas.base.BaseRelDTO):
     users: list[schemas.UserDTO]
 
 
-class UserSubscriptionRelDTO(schemas.UserSubscriptionDTO, schemas.base.RelDTO):
+class UserSubscriptionRelDTO(schemas.UserSubscriptionDTO, schemas.base.BaseRelDTO):
     user: schemas.UserDTO
     subscription: schemas.SubscriptionDTO
